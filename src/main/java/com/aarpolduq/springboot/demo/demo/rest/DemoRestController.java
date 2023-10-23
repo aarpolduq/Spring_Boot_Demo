f434c5d3d9815e68212c316506f9a372c30a569c
Aaron P. <aarpolduq@gmail.com>
2023-10-23 20:08:08 +0200

    RestController GET / HelloWorld

---
+++ src/main/java/com/aarpolduq/springboot/demo/demo/rest/DemoRestController.java
@@ -0,0 +1,15 @@
+package com.aarpolduq.springboot.demo.demo.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+
+    //expose "/" that returns "Hello World!"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
