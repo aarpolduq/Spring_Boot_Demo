7da0463f5df78ecf9162162b95563f9a52fc65d9
Aaron P. <aarpolduq@gmail.com>
2023-10-27 18:36:28 +0200

    package jar + properties

--- src/main/java/com/aarpolduq/springboot/demo/demo/rest/DemoRestController.java
+++ src/main/java/com/aarpolduq/springboot/demo/demo/rest/DemoRestController.java
@@ -1,11 +1,18 @@
 package com.aarpolduq.springboot.demo.demo.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     //expose "/" that returns "Hello World!"
     @GetMapping("/")
@@ -16,6 +23,10 @@ public class DemoRestController {
     @GetMapping("/workout")
     public String getDailyWorkout() {
          return "Run 5km!";
+    }
 
+    @GetMapping("/mickey")
+    public String getMickey() {
+        return "Hello "+coachName+" from "+ teamName;
     }
 }
