f5550eabedb48fee8b1c7d3183ff4d5d150f8516
Aaron P <aarpolduq@gmail.com>
2023-11-05 21:10:18 +0100

    refactoring

---
+++ 00-spring-boot-overview/src/main/java/com/aarpolduq/springboot/demo/demo/overview/rest/DemoRestController.java
@@ -0,0 +1,32 @@
+package com.aarpolduq.springboot.demo.demo.overview.rest;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose "/" that returns "Hello World!"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+         return "Run 5km!";
+    }
+
+    @GetMapping("/mickey")
+    public String getMickey() {
+        return "Hello "+coachName+" from "+ teamName;
+    }
+}
